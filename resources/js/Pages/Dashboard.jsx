395710b72dbb8cc3d6973a1b6e8a5e8ab82d5315
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-05-10 14:40:59 +0100

    instalacion de los packetes para crear y mostrar pdfs

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,29 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 import { Head } from '@inertiajs/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+
+/* function PDFViewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="ruta/al/archivo.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+} */
 
 export default function Dashboard({ auth }) {
     return (
