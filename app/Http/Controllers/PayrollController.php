86221c8280dc561b6ebc8dfff897d0940b6d3170
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-05-17 13:35:15 +0100

    mostrar el pdf en un modal

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -2,15 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Dompdf\Dompdf;
+use Inertia\Inertia;
+
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Session;
+
 
 
 class PayrollController extends Controller
 {
+    public function allPayrolls()
+    {
+    }
+
+
+
     public function generatePdf()
     {
+        Session::flash('success', 'Generating PDF!');
+
         $dompdf = new Dompdf();
+        $options = $dompdf->getOptions();
+
+        $dompdf->setOptions($options);
         $dompdf->loadHtml('<h1>Hello, World!</h1><p>This is your payroll</p>');
         $dompdf->setPaper('A4', 'portrait');
         $dompdf->render();
---
+++ resources/js/Components/FlashMessage.jsx
@@ -0,0 +1,40 @@
+import { usePage } from "@inertiajs/react";
+import Toast from 'react-bootstrap/Toast';
+import React, { useState } from 'react';
+
+export function dibujaFlash() {
+    const { flash } = usePage().props;
+    const [show, setShow] = useState(true);
+    let message;
+
+    if (flash.success) {
+        message = flash.success ?
+            <Toast
+                className="d-inline-block m-1 rounded-5"
+                bg='success'
+                key='alert_flash'
+                onClose={() => setShow(false)} show={show} delay={4050} autohide>
+                <div className="d-flex justify-content-between">
+                    <Toast.Body>
+                        <h5 className="fa fa-flash text-white">{flash.success}</h5>
+                    </Toast.Body>
+                    <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div>
+            </Toast> : ''
+        flash.success = null;
+
+    } else if (flash.error) {
+        message = flash.error ?
+            <Toast
+                className="d-inline-block m-1"
+                bg='danger'
+                key='alert_flash'
+                onClose={() => setShow(false)} show={show} delay={3500} autohide>
+                <Toast.Body>
+                    <strong className="fa fa-flash text-white">{flash.error}</strong>
+                </Toast.Body>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </Toast> : ''
+        flash.error = null;
+    }
+    return message;
+}
