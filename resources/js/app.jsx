41b85a7edd3dc445ec9443710534342b8bc0efa2
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-05-29 11:50:16 +0100

    preparando react para el despliegue

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,16 +6,17 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { Inertia } from '@inertiajs/inertia'; // Importa Inertia desde @inertiajs/inertia
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
 
-        root.render(<App {...props} />);
+    setup({ el, App, props }) {
+        constroot = createRoot(el);
+        root.render(<App{...props} />);
     },
     progress: {
         color: '#4B5563',
