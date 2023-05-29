4ae44f8bac9868041e2c5b490760af55cf7aec2c
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-05-29 16:21:11 +0100

    problema con el modelo

--- database/seeders/PayrollSeeder.php
+++ database/seeders/PayrollSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Payroll;
+use App\Models\payroll;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
