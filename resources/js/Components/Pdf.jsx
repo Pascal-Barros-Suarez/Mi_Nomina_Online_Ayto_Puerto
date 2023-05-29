f0ab287264220d3d6980c1d309dff46a7349348b
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-05-29 10:56:28 +0100

    identacion del codigo - proyecto terminado, - pasando fase despliegue

--- resources/js/Components/Pdf.jsx
+++ resources/js/Components/Pdf.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.js';
 const PdfViewer = (props) => {
   return (
     <object data={props.pdfData} type="application/pdf" className='w-100' height='87%'></object>
-    );
+  );
 };
 
 export default PdfViewer; 
